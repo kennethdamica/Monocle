f2dd28326c3e328199260ec876d8e7f4bf6ec116
Kenny <k.damica@gmail.com>
2013-09-25 18:46:16 -0700

    added license

--- monocle.lua
+++ monocle.lua
@@ -1,3 +1,18 @@
+[[Copyright (C) 2013 Kenneth J. D'Amica
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN 
+NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.]]
 
 local index = require 'index'
 local tuple = require 'tuple'
@@ -264,7 +279,6 @@ function Monocle:draw_triangles()
 		if self.edges[current_edge] then
 			current_edge = self.edges[current_edge][2]
 		else
-			print(current_edge)
 			break
 		end
 		count = count + 1
